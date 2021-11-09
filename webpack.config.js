3ec1275a8c980cd3254a32c4521442fe052ad30c
Lucas Cunha <you@example.com>
2021-11-09 02:17:33 -0300

    initial webpack configure

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+            },
+          ],
+        },
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+          },
+      ],
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+          template: './public/index.html',
+          filename: './index.html',
+          favicon: './public/favicon.ico',
+        })
+      ],
+  };
