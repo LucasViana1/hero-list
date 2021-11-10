97447dce2f7e7d606b6a160cff589d5cef20781e
Lucas Cunha <you@example.com>
2021-11-09 21:37:27 -0300

    update the global style: background and line height

--- src/styles/global.js
+++ src/styles/global.js
@@ -8,8 +8,12 @@ export default createGlobalStyle`
     box-sizing: 0;
   }
 
-  body{
-    background-color: #f3f3f3;
+  body {
+    background-color: #fff;
     font-family: 'Work Sans', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, p {
+    line-height: 1.5;
   }
 `;
