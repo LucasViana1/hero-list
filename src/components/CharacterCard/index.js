72d6f6de8163cd4d242ee9477fdb70994578a4e6
Lucas Cunha <you@example.com>
2021-11-10 11:57:40 -0300

    create the ChatacterCard component

---
+++ src/components/CharacterCard/index.js
@@ -0,0 +1,45 @@
+import { memo, useState } from 'react';
+import PropTypes from 'prop-types';
+import iconHeartUnfilled from '@assets/icones/heart/Path Copy 2@2x.png';
+import iconHeartFilled from '@assets/icones/heart/Path Copy 7@2x.png';
+import * as S from './styles';
+import Button from '../Button';
+
+const CharacterCard = ({ character }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const {
+    name,
+    thumbnail: { extension, path },
+  } = character;
+
+  const handleAddToFavorite = () => {
+    setIsFavorite((oldIsFavorite) => !oldIsFavorite);
+  };
+
+  return (
+    <S.CharacterCard>
+      <img src={`${path}.${extension}`} alt={name} />
+
+      <S.CharacterInfos>
+        <p title={name}>{name}</p>
+        <Button onClick={handleAddToFavorite} minWidth="1rem">
+          <img src={isFavorite ? iconHeartFilled : iconHeartUnfilled} alt="Favoritar" />
+        </Button>
+      </S.CharacterInfos>
+    </S.CharacterCard>
+  );
+};
+
+CharacterCard.propTypes = {
+  character: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    thumbnail: PropTypes.shape({
+      extension: PropTypes.string.isRequired,
+      path: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
+
+export default memo(CharacterCard);
---
+++ src/components/CharacterCard/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+import { breakpoint } from '@styles/theme';
+
+export const CharacterCard = styled.div`
+  height: 20rem;
+  width: 18rem;
+  border: 1px solid black;
+  margin: 0 22px 4rem 0;
+
+  @media (min-width: ${breakpoint.xl}) {
+    width: 12rem;
+  }
+
+  img {
+    width: 100%;
+    height: 76%;
+  }
+`;
+
+export const CharacterInfos = styled.div`
+  margin: 0 0.25rem;
+
+  p {
+    text-align: center;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  img {
+    width: auto;
+    height: 2rem;
+  }
+`;
