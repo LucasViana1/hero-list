639392f93d6e57a9d986fe96f0546de60b36ef86
Lucas Cunha <you@example.com>
2021-11-11 01:48:02 -0300

    input with differents kinds

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,9 +2,9 @@ import PropTypes from 'prop-types';
 import iconBusca from '@assets/busca/Lupa/Shape.png';
 import * as S from './styles';
 
-const Input = ({ id, type, placeholder, onChange }) => (
-  <S.InputContainer>
-    <img src={iconBusca} alt="Busca" />
+const Input = ({ id, type, kind, placeholder, onChange }) => (
+  <S.InputContainer kind={kind}>
+    {type === 'search' && <img src={iconBusca} alt="Busca" />}
     <input id={id} type={type} placeholder={placeholder} onChange={onChange} />
   </S.InputContainer>
 );
@@ -12,12 +12,14 @@ const Input = ({ id, type, placeholder, onChange }) => (
 Input.propTypes = {
   id: PropTypes.string.isRequired,
   type: PropTypes.oneOf(['text', 'search']),
+  kind: PropTypes.oneOf(['light', 'danger']),
   placeholder: PropTypes.string,
   onChange: PropTypes.func.isRequired,
 };
 
 Input.defaultProps = {
   type: 'text',
+  kind: 'danger',
   placeholder: '',
 };
 
--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,11 +1,21 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { color } from '@styles/theme';
+
+const theme = {
+  danger: { background: color.red.light, color: color.red.normal },
+  light: { background: '#fff', color: color.gray.normal },
+};
 
 export const InputContainer = styled.label`
   padding: 1rem 1.5rem;
   display: flex;
   border-radius: 2rem;
-  background: ${color.red.light};
+  background: ${({ kind }) => theme[kind].background};
+  ${({ kind }) =>
+    kind === 'light' &&
+    css`
+      border: 1px solid ${color.gray.light};
+    `};
 
   img {
     margin-right: 2rem;
@@ -16,11 +26,11 @@ export const InputContainer = styled.label`
     border: 0;
     outline: 0;
     width: 100%;
-    color: ${color.red.normal};
+    color: ${({ kind }) => theme[kind].color};
     font-size: 1rem;
 
     ::placeholder {
-      color: ${color.red.normal};
+      color: ${({ kind }) => theme[kind].color};
       font-weight: 700;
       font-size: 1rem;
     }
