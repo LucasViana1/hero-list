0f3753e3c6bdd97150675bf1264dcd21e7f9df2c
Lucas Cunha <you@example.com>
2021-11-11 21:37:18 -0300

    added the prop size in Loader component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,5 +1,14 @@
+import PropTypes from 'prop-types';
 import * as S from './styles';
 
-const Loader = () => <S.Loader />;
+const Loader = ({ size }) => <S.Loader size={size} />;
+
+Loader.propTypes = {
+  size: PropTypes.oneOf(['small', 'medium']),
+};
+
+Loader.defaultProps = {
+  size: 'medium',
+};
 
 export default Loader;
--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -1,6 +1,11 @@
 import styled, { keyframes } from 'styled-components';
 import { color } from '@styles/theme';
 
+const sizes = {
+  small: '2rem',
+  medium: '8rem',
+};
+
 const spin = keyframes`
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
@@ -12,8 +17,9 @@ export const Loader = styled.div`
   border-radius: 50%;
   border: 4px solid transparent;
   border-top-color: ${color.gray.normal};
+  margin: 0.5rem;
   animation: ${spin} 0.8s linear infinite;
 
-  width: 8rem;
-  height: 8rem;
+  width: ${({ size }) => sizes[size]};
+  height: ${({ size }) => sizes[size]};
 `;
