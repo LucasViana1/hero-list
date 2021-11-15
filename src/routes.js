f9fa77c3697fd01b1fe5bc6069a34755d27c6b23
Lucas Cunha <you@example.com>
2021-11-14 22:48:14 -0300

    change for hash router

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Character from './pages/Character';
