4d4fa359932790227f311eb011cabe1e5117bb79
Lucas Cunha <you@example.com>
2021-11-14 22:43:51 -0300

    public path in webpack

--- webpack.common.js
+++ webpack.common.js
@@ -68,6 +68,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
   devServer: {
     static: './dist',
